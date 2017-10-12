7e19dbaa2fa0f4208292f59508f39d0a4987ac2c
anaimhunter <anaimhunter@hotmail.com>
2017-10-12 10:47:44 -0500

    Initial commit

---
+++ src/EjemploExcepciones.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Ana M.Hunter on 12/10/2017.
+ */
+public class EjemploExcepciones
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hola");
+    }
+}
